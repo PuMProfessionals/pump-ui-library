414b67a5c0b2bf93d7fc8d970926ddc2a3ac0bcf
xsharonhe <xsharonhe@gmail.com>
2021-03-05 18:32:04 -0500

    Reusable text component

---
+++ src/components/Texts/Text/Text.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Text = ({
+    children,
+    color = 'primary',
+    size = 'default',
+    lineHeight = '1.25',
+    padding,
+    ...props
+}) => (
+    <SText size={size} color={color} lineHeight={lineHeight} {...props}>
+        {children}
+    </SText>
+);
+
+const SText = styled.p`
+    ${({ theme, bold, lineHeight, color = 'text', size = 'default' }) => `
+        color: ${theme.colors[color] || color};
+        font-size: ${theme.size[size] || size};
+        line-height: ${lineHeight || 1.25};
+        font-weight: ${bold ? 'bold' : 'normal'};
+    `}
+`;
---
+++ src/components/Texts/Text/Text.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text } from './Text';
+import styled from 'styled-components';
+
+export default {
+  title: 'Example/Text',
+  component: Text,
+};
+
+const Template = (args) => (
+    <SText {...args}> Title </SText>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+    lineHeight: 1.25,
+    bold: true,
+    size: 'h1'
+};
+
+const SText = styled(Text)`
+    padding: 0 30px;
+`;
