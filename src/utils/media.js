ea2f8558a684ee0e013ca3dfa0cafcce57dc27c8
xsharonhe <xsharonhe@gmail.com>
2021-06-24 04:51:37 -0400

    Fix some files

--- src/utils/media.js
+++ src/utils/media.js
@@ -3,7 +3,10 @@
    @param {string, number} breakpoint - string for themes, numbere for px for breakpoint
    @param {string} styles - css styles wanted to add
  */
-export const media = (breakpoint, styles) => ({ theme }) => `
+export const media =
+  (breakpoint, styles) =>
+  ({ theme }) =>
+    `
     @media (max-width: ${theme.media[breakpoint] || breakpoint}px) {
             ${styles}
 }`;
