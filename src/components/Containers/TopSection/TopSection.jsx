3ac3e13411162b4efd48faa03a9b75173de332f1
noisya55616 <46069730+noisya55616@users.noreply.github.com>
2021-07-07 21:05:57 -0400

    Add adjustments for mobile displays

--- src/components/Containers/TopSection/TopSection.jsx
+++ src/components/Containers/TopSection/TopSection.jsx
@@ -2,51 +2,70 @@ import React from "react";
 import styled from "styled-components";
 
 import SideGraphic from "../../../assets/top-section-graphic.svg";
-//import { media } from "../../../utils";
+import { media } from "../../../utils";
 import { Text } from "../../Texts";
 import { Button } from "../../Inputs";
 
 export const TopSection = ({ ...props }) => (
-    <MainContainer {...props}>
-        <FirstColumn>
-              <JosefinTitle size="xxlarge" bold="true" >Prospective Medical Professionals</JosefinTitle>
-              <SText size="default" >A registered charity dedicated to connecting aspiring students to health sciences opportunities</SText>
-              <SButton>DONATE</SButton>
-        </FirstColumn>
-        <SecondColumn>
-            <Graphic src={SideGraphic} alt="Graphic" />
-        </SecondColumn>
-    </MainContainer>
+  <MainContainer {...props}>
+    <MobileGraphic>
+      <Graphic src={SideGraphic} alt="Graphic" />
+    </MobileGraphic>
+    <FirstColumn>
+      <JosefinTitle size="xxlarge" bold="true" >Prospective Medical Professionals</JosefinTitle>
+      <SText size="default" >A registered charity dedicated to connecting aspiring students to health sciences opportunities</SText>
+      <SButton><ButtonText size="default" bold="true" >DONATE</ButtonText></SButton>
+    </FirstColumn>
+      <SecondColumn>
+        <Graphic src={SideGraphic} alt="Graphic" />
+      </SecondColumn>
+  </MainContainer>
 );
 
 const MainContainer = styled.div`
-  ${({ theme }) => `
-        display: flex;
-        grid-template-columns: repeat(3, 1fr);
-        margin: 5em;
-        font-family: ${theme.font.lato};
-        font-size: ${theme.size.default};
-        {/*background-color: ${theme.colors.navy};*/}
-        color: ${theme.colors.navy};
-    `};
+  display: flex;
+  margin: 5em;
+  ${media(
+    "mobile",
+    `
+        margin: 1em;
+        flex-direction: column;
+        `
+  )};
 `;
 
 const SText = styled(Text)`
   ${({ theme }) => `
-        color: ${theme.colors.navy};
-        margin-top: 0;
-    `};
+    font-family: ${theme.font.lato};
+    color: ${theme.colors.navy};
+    margin-top: 0;
+  `};
+  ${media(
+    "mobile",
+    `
+        line-height: 1.3;
+        `
+  )};
 `;
 
 const JosefinTitle = styled(SText)`
   ${({ theme }) => `
-        font-family: ${theme.font.josefin};
-        margin: 2em 0 0 0;
-    `};
+    font-family: ${theme.font.josefin};
+    margin: 1em 0 0 0;
+  `};
+  ${media(
+    "mobile",
+    `
+        font-size: 2.5rem;
+        `
+  )};
+`;
+
+const ButtonText = styled(SText)`
+  margin: 0 0 0 0;
 `;
 
 const Graphic = styled.img`
-  {/*width: 300px;*/}
   width: 100%;
   height: auto;
 `;
@@ -55,19 +74,34 @@ const SButton = styled(Button)`
 `;
 
 const FirstColumn = styled.div`
-  {/*
-  grid-column: 1 / 3;
-  padding: 130px 20px 100px 100px;
-  */}
-  flex: 0 0 40%;
+  flex: 0 0 35%;
+  ${media(
+    "mobile",
+    `
+        flex: 1 0 0;
+        `
+  )};
 `;
 
 const SecondColumn = styled.div`
-  {/*
-  padding: 100px 100px 100px 100px;
-  width: 50%;
-  max-width: 50%;
-  */}
   padding-left: 10em;
   flex: 1;
+  ${media(
+    "mobile",
+    `
+        flex: 0 0 0;
+        display: none;
+        `
+  )};
+`;
+
+const MobileGraphic = styled(FirstColumn)`
+    display: none;
+    width: 100%;
+    ${media(
+      "mobile",
+      `
+          display: block;
+          `
+    )};
 `;
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -14,7 +14,8 @@ export const baseTheme = {
     h3: "1.3rem",
     defaultLarger: "1.5rem",
     large: "2.5rem",
-    xxlarge: "10rem",
+    xlarge: "3em",
+    xxlarge: "4.5rem",
   },
   radius: {
     default: "8px",
