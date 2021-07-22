7b5e0e6ac4b0a813cecd7d3d701ea476e1f81005
hirundinidae <diana4lin@gmail.com>
2021-07-22 15:51:39 -0400

    Add text and image

---
+++ src/components/Containers/StudentResource/StudentResource.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+import { Text } from "../../Texts";
+export const StudentResource = ({ titleText, descriptionText, graphic, ...props }) => (
+  <StudentResourceSection {...props}>
+    <InfoSection>
+      <Title>{titleText}</Title>
+      {/* TODO: Description */}
+      {/* TODO: Button array (color, link, text) */}
+      <Text>{descriptionText}</Text>
+    </InfoSection>
+      <Graphic src={graphic} />
+  </StudentResourceSection>
+);
+
+const Title = styled.h2`
+  ${({ theme }) => `
+      font-family: ${theme.font.josefin};
+      color: ${theme.colors.black};
+  `};
+`;
+
+const Graphic = styled.img`
+  width: 550px; /* TODO: maybe width and height prop? */
+  height: 350px;
+  height: 350px;
+  border: 2px solid black; /* TODO: curved border */
+`;
+
+const StudentResourceSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* TODO: flex column on mobile */
+`;
+
+const InfoSection = styled.div`
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 5%; /* TODO: ternary for image location, margin-left */
+  /* TODO: change text color based on background */
+`;
+
+StudentResource.propTypes = {
+  titleText: PropTypes.string,
+  descriptionText: PropTypes.string,
+  graphic: PropTypes.string
+};
---
+++ src/components/Containers/StudentResource/StudentResource.stories.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StudentResource } from "./StudentResource";
+import PlaceholderGraphic from "../../../assets/introductions-mdx/code-brackets.svg";
+
+export default {
+  title: "Containers/StudentResource",
+  component: StudentResource
+};
+
+const Template = args => <StudentResource {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  titleText: "PuMP Direct",
+  descriptionText:
+    "Browse and filter our database of local opportunities in healthcare",
+  graphic: PlaceholderGraphic
+};
